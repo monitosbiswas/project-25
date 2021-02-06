53ecc98746815ef851e1e7b5bd4a991d7e4a43cb
monitosh biswas <74135155+monitosbiswas@users.noreply.github.com>
2021-02-06 11:33:28 +0530

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,38 @@
+class dustbin
+{
+	constructor(x,y)
+	{
+		this.x=x;
+		this.y=y;
+		this.dustbinWidth=200;
+		this.dustbinHeight=213;
+		this.wallThickness=20;
+		
+		this.bottomBody=Bodies.rectangle(this.x-this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true})
+		this.leftWallBody=Bodies.rectangle(this.x-this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true})
+		
+		this.image=loadImage("dustbingreen.png");
+
+		this.rightWallBody=Bodies.rectangle(this.x+this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true})
+		
+		World.add(world, this.bottomBody)
+		World.add(world, this.leftWallBody)
+		World.add(world, this.rightWallBody);
+
+	}
+	display()
+	{
+			var posBottom=this.bottomBody.position;
+
+			push()
+			translate(posBottom.x+20, posBottom.y-90);
+			rectMode(CENTER)
+			strokeWeight(4);
+			angleMode(RADIANS)
+			fill(255)
+			image(this.image,0,0,this.dustbinWidth, this.dustbinHeight);
+			pop()
+			
+	}
+
+}
